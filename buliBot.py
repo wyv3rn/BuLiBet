11d7e4f494fecd8780e9c3ca78817db7078eb9f8
David Schatz <david.schatz@tu-ilmenau.de>
2018-10-11 22:04:38 +0200

    Reworked bet button design

--- buliBot.py
+++ buliBot.py
@@ -11,6 +11,29 @@ import re
 
 log.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=log.INFO)
 
+team_short = {
+    'Frankfurt': 'EIN',
+    'Düsseldorf': 'DÜS',
+    'Leverkusen': 'LEV',
+    'Hannover': 'H96',
+    'Stuttgart': 'VfB',
+    'Dortmund': 'BVB',
+    'Augsburg': 'FCA',
+    'Leipzig': 'RBL',
+    'Wolfsburg': 'VfL',
+    'Bayern': 'FCB',
+    'Nürnberg': 'FCN',
+    'Hoffenheim': 'TSG',
+    'Schalke': 'S04',
+    'Bremen': 'SVB',
+    'Hertha': 'BSC',
+    'Freiburg': 'SCF',
+    'Gladbach': 'BMG',
+    'Mainz': 'FSV'
+}
+
+HIGHEST_INC = 2
+
 def fetch_matchday(day):
     global matchday
     matchday = []
@@ -22,7 +45,6 @@ def fetch_matchday(day):
     teams = []
     for result in re.finditer(r'<div class="ovVrnLink2015">\r\n\r\n.*\r\n.*class=".*">(?P<team>.*)</a>\r\n\r\n</div>', contents):
         team = result.group('team')
-        print(team)
         teams.append(team)
         if len(teams) == 2:
             # match complete
@@ -38,22 +60,30 @@ class Match:
         self.home_score = 0
         self.guest_score = 0
 
-    def to_buttons(self, row):
-        buttons = [InlineKeyboardButton('-', callback_data='{} -home'.format(row)),
-                    InlineKeyboardButton(self.home, callback_data='{} +home'.format(row)),
-                    InlineKeyboardButton('{} : {}'.format(self.home_score, self.guest_score), callback_data='nop'.format(row)),
-                    InlineKeyboardButton(self.guest, callback_data='{} +guest'.format(row)),
-                    InlineKeyboardButton('-', callback_data='{} -guest'.format(row))]
+    def to_inc_buttons(self, row, highest_inc=HIGHEST_INC):
+        buttons = []
+        inc = highest_inc
+        while inc > 0:
+            buttons.append(InlineKeyboardButton('+{}'.format(inc), callback_data='{} +{} home'.format(row, inc)))
+            inc -= 1
+
+        buttons.append(InlineKeyboardButton(team_short[self.home], callback_data='{} reset home'.format(row)))
+        buttons.append(InlineKeyboardButton('{} : {}'.format(self.home_score, self.guest_score), callback_data='nop'.format(row)))
+        buttons.append(InlineKeyboardButton(team_short[self.guest], callback_data='{} reset guest'.format(row)))
+
+        while inc < highest_inc:
+            inc += 1
+            buttons.append(InlineKeyboardButton('+{}'.format(inc), callback_data='{} +{} guest'.format(row, inc)))
         return buttons
 
     def to_string(self):
         return '{} {}:{} {}'.format(self.home, self.home_score, self.guest_score, self.guest)
 
 
-def matchday_to_keyboard(matchday):
+def matchday_to_inc_keyboard(matchday):
     keyboard = []
     for i in range(len(matchday)):
-        keyboard.append(matchday[i].to_buttons(i))
+        keyboard.append(matchday[i].to_inc_buttons(i))
     keyboard.append([InlineKeyboardButton('Done 🙈', callback_data='done')])
     return keyboard
 
@@ -66,29 +96,25 @@ def start(bot, update):
     log.log(log.INFO, 'Received start command message {}'.format(update.message))
     bot.send_message(chat_id=update.message.chat_id, text='You wanna start? But I\'m not ready yet :(')
 
-# TODO testing only
-def key(bot, update):
-    global matchday
-    match0 = Match('FCB', 'FCN')
-    match1 = Match('BVB', 'S04')
-    matchday = [match0, match1]
-
-    reply_markup = InlineKeyboardMarkup(matchday_to_keyboard(matchday))
-    update.message.reply_text('Here we go:', reply_markup=reply_markup)
-
 def bet(bot, update, args):
+    # TODO use database instead of one global variable ...
+    # or just parse buttons again? and only save in database after user is "done"
     global matchday
     # TODO check args
     day = int(args[0])
     fetch_matchday(day)
 
-    reply_markup = InlineKeyboardMarkup(matchday_to_keyboard(matchday))
-    update.message.reply_text('Here we go:', reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup(matchday_to_inc_keyboard(matchday))
+    update.message.reply_text('Spieltag {}:'.format(day), reply_markup=reply_markup)
 
 
 def button(bot, update):
     global matchday
     query = update.callback_query
+    username = query.from_user.username
+    user_id = query.from_user.id
+    msg = query.message.text
+    log.log(log.INFO, 'Received button callback from user {} ({}), base message "{}"'.format(username, user_id, msg))
 
     if query.data == 'nop':
         return
@@ -99,26 +125,33 @@ def button(bot, update):
                         text=matchday_to_string(matchday))
         return
 
-    row, command = query.data.split(' ')
-    match = matchday[int(row)]
-    if command == '+home':
-        match.home_score += 1
-    elif command == '-home':
-        if match.home_score > 0:
-            match.home_score -= 1
-        else:
-            return
-    elif command == '+guest':
-        match.guest_score += 1
-    elif command == '-guest':
-        if match.guest_score > 0:
-            match.guest_score -= 1
-        else:
-            return
-    else:
-        assert False, 'Unexpected button callback {}'.format(command)
+    row, command, arg = query.data.split(' ')
+    if arg not in ['home', 'guest']:
+        log.log(log.WARNING, 'FIXME: Unexpected argument: {}'.format(arg))
+        return
 
-    reply_markup = InlineKeyboardMarkup(matchday_to_keyboard(matchday))
+    match = matchday[int(row)]
+    if command == 'reset':
+        if arg == 'home':
+            if match.home_score == 0:
+                return
+            match.home_score = 0
+        else:
+            if match.guest_score == 0:
+                return
+            match.guest_score = 0
+    else:
+        inc_re = re.match(r'\+(?P<inc>\d+)', command)
+        if not inc_re:
+            log.log(log.WARNING, 'Unexpected command: {}'.format(command))
+            return
+        inc = int(inc_re.group('inc'))
+        if arg == 'home':
+            match.home_score += inc
+        else:
+            match.guest_score += inc
+
+    reply_markup = InlineKeyboardMarkup(matchday_to_inc_keyboard(matchday))
 
     bot.edit_message_reply_markup(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
@@ -131,7 +164,6 @@ def main(token, storage):
     log.log(log.INFO, 'Bot controller started')
 
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('key', key))
     dispatcher.add_handler(CommandHandler('bet', bet, pass_args=True))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
